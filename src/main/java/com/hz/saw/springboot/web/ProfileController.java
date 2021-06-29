da22a1b73cd82ac056abf4dc8486ffebd1d8460c
HEEJIN <heehee2310@gamil.com>
2021-06-29 17:24:31 +0900

    무중단 배포를 위한 profile API 및 테스트코드 생성

---
+++ src/main/java/com/hz/saw/springboot/web/ProfileController.java
@@ -0,0 +1,28 @@
+package com.hz.saw.springboot.web;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+public class ProfileController {
+    private final Environment env;
+
+    @GetMapping("/profile")
+    public String profile() {
+        // real, oauth, real-db 등이 활성화되어 있다면 3개가 모두 담겨 있음.
+        List<String> profiles = Arrays.asList(env.getActiveProfiles());
+        List<String> realProfiles = Arrays.asList("real", "real1", "real2"); // real, real1, real2는 배포에 사용될 profile
+        String defaultProfile = profiles.isEmpty()? "default" : profiles.get(0);
+
+        return profiles.stream()
+                .filter(realProfiles::contains)
+                .findAny()
+                .orElse(defaultProfile);
+    }
+}
